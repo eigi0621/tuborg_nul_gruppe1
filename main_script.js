e7a23f632bc80ce74ddb9cee17fa3d86b39413e9
eigi0621 <eigi0088@gmail.com>
2020-03-24 21:26:29 +0100

    data virker!!!!

--- main_script.js
+++ main_script.js
@@ -366,11 +366,21 @@ function meterAnimation() {
   }, 1000)
 }
 
-testYourself1();
+testYourself();
+let randomRunning = false;
+console.log(randomRunning);
 
-function testYourself1() {
-  var slider1 = document.querySelector("#slider1");
-  var vaerdi1 = document.querySelector("#vaerdi1");
+function testYourself() {
+  document.querySelectorAll(".slidecontainer").forEach((slcon) => {
+    slcon.addEventListener("mouseover", () => {
+      randomRunning = false;
+    })
+  })
+
+  var slider1a = document.querySelector("#slider1a");
+  var slider1b = document.querySelector("#slider1b");
+  var vaerdi1a = document.querySelector("#vaerdi1a");
+  var vaerdi1b = document.querySelector("#vaerdi1b");
   var slider2a = document.querySelector("#slider2a");
   var slider2b = document.querySelector("#slider2b");
   var vaerdi2a = document.querySelector("#vaerdi2a");
@@ -384,8 +394,10 @@ function testYourself1() {
     document.querySelectorAll("#barchart1 .bars line").forEach((bar) => {
       bar.style.transition = "0s cubic-bezier(0.76, 0, 0.24, 1)";
     })
-    document.querySelector("#barchart1 .bars line:nth-child(1)").style.transform = `translateY(${this.value / 10}px)`;
-    document.querySelector("#barchart1 .bars line:nth-child(2)").style.transform = `translateY(${100 - this.value / 10}px)`;
+    document.querySelector("#barchart1 .bars line:nth-child(2)").style.transform = `translateY(${this.value / 10}px)`;
+    document.querySelector("#barchart1 .bars line:nth-child(1)").style.transform = `translateY(${100 - this.value / 10}px)`;
+    document.querySelector("#barchart1 .bars line:nth-child(1)").style.stroke = "#093A82";
+    document.querySelector("#barchart1 .bars line:nth-child(2)").style.stroke = "#3c6196";
     vaerdi1a.textContent = `${Math.round(this.value / 10)}%`
     vaerdi1b.textContent = `${100 - Math.round(this.value / 10)}%`
     slider1b.value = `${1000 - this.value}`;
@@ -394,8 +406,10 @@ function testYourself1() {
     document.querySelectorAll("#barchart1 .bars line").forEach((bar) => {
       bar.style.transition = "0s cubic-bezier(0.76, 0, 0.24, 1)";
     })
-    document.querySelector("#barchart1 .bars line:nth-child(2)").style.transform = `translateY(${this.value / 10}px)`;
-    document.querySelector("#barchart1 .bars line:nth-child(1)").style.transform = `translateY(${100 - this.value / 10}px)`;
+    document.querySelector("#barchart1 .bars line:nth-child(1)").style.transform = `translateY(${this.value / 10}px)`;
+    document.querySelector("#barchart1 .bars line:nth-child(2)").style.transform = `translateY(${100 - this.value / 10}px)`;
+    document.querySelector("#barchart1 .bars line:nth-child(1)").style.stroke = "#093A82";
+    document.querySelector("#barchart1 .bars line:nth-child(2)").style.stroke = "#3c6196";
     vaerdi1b.textContent = `${Math.round(this.value / 10)}%`
     vaerdi1a.textContent = `${100 - Math.round(this.value / 10)}%`
     slider1a.value = `${1000 - this.value}`;
@@ -404,65 +418,87 @@ function testYourself1() {
     document.querySelectorAll("#barchart2 .bars line").forEach((bar) => {
       bar.style.transition = "0s cubic-bezier(0.76, 0, 0.24, 1)";
     })
-    document.querySelector("#barchart2 .bars line:nth-child(1)").style.transform = `translateY(${this.value / 10 + 52.1}px)`;
-    document.querySelector("#barchart2 .bars line:nth-child(2)").style.transform = `translateY(${47.9 - (this.value / 10) + 52.1}px)`;
+    document.querySelector("#barchart2 .bars line:nth-child(2)").style.transform = `translateY(${this.value / 10 + 52.1}px)`;
+    document.querySelector("#barchart2 .bars line:nth-child(1)").style.transform = `translateY(${47.9 - (this.value / 10) + 52.1}px)`;
+    document.querySelector("#barchart2 .bars line:nth-child(1)").style.stroke = "#093A82";
+    document.querySelector("#barchart2 .bars line:nth-child(2)").style.stroke = "#3c6196";
     vaerdi2a.textContent = `${Math.round(this.value / 10)}%`
     vaerdi2b.textContent = `${Math.round(47.9 - (this.value / 10))}%`
     slider2b.value = `${479 - this.value}`;
   }
   slider2b.oninput = function () {
-    document.querySelectorAll("#barchart1 .bars line").forEach((bar) => {
+    document.querySelectorAll("#barchart2 .bars line").forEach((bar) => {
       bar.style.transition = "0s cubic-bezier(0.76, 0, 0.24, 1)";
     })
-    document.querySelector("#barchart2 .bars line:nth-child(2)").style.transform = `translateY(${this.value / 10}px)`;
-    document.querySelector("#barchart2 .bars line:nth-child(1)").style.transform = `translateY(${100 - this.value / 10}px)`;
+    document.querySelector("#barchart2 .bars line:nth-child(1)").style.transform = `translateY(${this.value / 10 + 52.1}px)`;
+    document.querySelector("#barchart2 .bars line:nth-child(2)").style.transform = `translateY(${47.9 - (this.value / 10) + 52.1}px)`;
+    document.querySelector("#barchart2 .bars line:nth-child(1)").style.stroke = "#093A82";
+    document.querySelector("#barchart2 .bars line:nth-child(2)").style.stroke = "#3c6196";
     vaerdi2b.textContent = `${Math.round(this.value / 10)}%`
-    vaerdi2a.textContent = `${47.9 - Math.round(this.value / 10)}%`
+    vaerdi2a.textContent = `${Math.round(47.9 - (this.value / 10))}%`
     slider2a.value = `${479 - this.value}`;
   }
   slider3a.oninput = function () {
     document.querySelectorAll("#barchart3 .bars line").forEach((bar) => {
       bar.style.transition = "0s cubic-bezier(0.76, 0, 0.24, 1)";
     })
-    document.querySelector("#barchart3 .bars line:nth-child(1)").style.transform = `translateY(${100 - this.value / 10}px)`;
+    document.querySelector("#barchart3 .bars line:nth-child(2)").style.transform = `translateY(${this.value / 10 + 7.9}px)`;
+    document.querySelector("#barchart3 .bars line:nth-child(1)").style.transform = `translateY(${92.3 - (this.value / 10) + 7.7}px)`;
+    document.querySelector("#barchart3 .bars line:nth-child(1)").style.stroke = "#093A82";
+    document.querySelector("#barchart3 .bars line:nth-child(2)").style.stroke = "#3c6196";
     vaerdi3a.textContent = `${Math.round(this.value / 10)}%`
+    vaerdi3b.textContent = `${Math.round(92.3 - (this.value / 10))}%`
+    slider3b.value = `${923 - this.value}`;
   }
   slider3b.oninput = function () {
-    document.querySelector("#barchart3 .bars line:nth-child(2)").style.transform = `translateY(${100 - this.value / 10}px)`;
+    document.querySelectorAll("#barchart3 .bars line").forEach((bar) => {
+      bar.style.transition = "0s cubic-bezier(0.76, 0, 0.24, 1)";
+    })
+    document.querySelector("#barchart3 .bars line:nth-child(1)").style.transform = `translateY(${this.value / 10 + 7.9}px)`;
+    document.querySelector("#barchart3 .bars line:nth-child(2)").style.transform = `translateY(${92.3 - (this.value / 10) + 7.7}px)`;
+    document.querySelector("#barchart3 .bars line:nth-child(1)").style.stroke = "#093A82";
+    document.querySelector("#barchart3 .bars line:nth-child(2)").style.stroke = "#3c6196";
     vaerdi3b.textContent = `${Math.round(this.value / 10)}%`
+    vaerdi3a.textContent = `${Math.round(92.3 - (this.value / 10))}%`
+    slider3a.value = `${923 - this.value}`;
   }
 
+
+
+
+
+
+
   document.querySelector("#test1").addEventListener("click", () => {
-    const valuesChart1 = [69.4, 30.6];
+    const valuesChart1 = [30.6, 69.4];
     document.querySelectorAll("#barchart1 .bars line").forEach((bar, i) => {
       bar.style.transition = "1s cubic-bezier(0.76, 0, 0.24, 1)";
       bar.style.transform = `translateY(${valuesChart1[i]}px)`;
-      console.log(i);
     })
+    document.querySelector("#barchart1 .bars line:nth-child(1)").style.stroke = "#FAC177";
+    document.querySelector("#barchart1 .bars line:nth-child(2)").style.stroke = "#ffe2bb";
 
     let bar1Loop = true;
 
     setInterval(function () {
       if (bar1Loop == true) {
-        let bar1a = document.querySelector("#barchart1 .bars line:nth-child(1)");
+        let bar1a = document.querySelector("#barchart1 .bars line:nth-child(2)");
         let comp1a = window.getComputedStyle(bar1a).getPropertyValue("transform");
 
         var values1a = comp1a.split('(')[1];
         values1a = values1a.split(')')[0];
         values1a = values1a.split(', ');
         var bar1aVal = values1a[5];
-        console.log(bar1aVal);
         vaerdi1a.textContent = `${Math.round(bar1aVal)}%`;
         slider1a.value = `${(bar1aVal * 10)}`;
 
-        let bar1b = document.querySelector("#barchart1 .bars line:nth-child(2)");
+        let bar1b = document.querySelector("#barchart1 .bars line:nth-child(1)");
         let comp1b = window.getComputedStyle(bar1b).getPropertyValue("transform");
 
         var values1b = comp1b.split('(')[1];
         values1b = values1b.split(')')[0];
         values1b = values1b.split(', ');
         var bar1bVal = values1b[5];
-        console.log(bar1bVal);
         vaerdi1b.textContent = `${Math.round(bar1bVal)}%`;
         slider1b.value = `${(bar1bVal * 10)}`;
       }
@@ -477,8 +513,9 @@ function testYourself1() {
     document.querySelectorAll("#barchart2 .bars line").forEach((bar, i) => {
       bar.style.transition = "1s cubic-bezier(0.76, 0, 0.24, 1)";
       bar.style.transform = `translateY(${100 - valuesChart2[i]}px)`;
-      console.log(i);
     })
+    document.querySelector("#barchart2 .bars line:nth-child(1)").style.stroke = "#FAC177";
+    document.querySelector("#barchart2 .bars line:nth-child(2)").style.stroke = "#ffe2bb";
 
     let bar2Loop = true;
 
@@ -491,20 +528,18 @@ function testYourself1() {
         values2a = values2a.split(')')[0];
         values2a = values2a.split(', ');
         var bar2aVal = values2a[5];
-        console.log(bar2aVal);
-        vaerdi2a.textContent = `${Math.round(bar2aVal)}%`;
-        slider2a.value = `${(bar2aVal * 10)}`;
+        vaerdi2a.textContent = `${Math.round(bar2aVal - 52.1)}%`;
+        slider2a.value = `${(bar2aVal * 10) - 521}`;
 
-        let bar2b = document.querySelector("#barchart2 .bars line:nth-child(2)");
+        let bar2b = document.querySelector("#barchart2 .bars line:nth-child(1)");
         let comp2b = window.getComputedStyle(bar2b).getPropertyValue("transform");
 
         var values2b = comp2b.split('(')[1];
         values2b = values2b.split(')')[0];
         values2b = values2b.split(', ');
         var bar2bVal = values2b[5];
-        console.log(bar2bVal);
-        vaerdi2b.textContent = `${Math.round(bar2bVal)}%`;
-        slider2b.value = `${(bar2bVal * 10)}`;
+        vaerdi2b.textContent = `${Math.round(bar2bVal - 52.1)}%`;
+        slider2b.value = `${(bar2bVal * 10) - 521}`;
       }
     }, 20)
 
@@ -517,34 +552,33 @@ function testYourself1() {
     document.querySelectorAll("#barchart3 .bars line").forEach((bar, i) => {
       bar.style.transition = "1s cubic-bezier(0.76, 0, 0.24, 1)";
       bar.style.transform = `translateY(${100 - valuesChart3[i]}px)`;
-      console.log(i);
     })
+    document.querySelector("#barchart3 .bars line:nth-child(1)").style.stroke = "#FAC177";
+    document.querySelector("#barchart3 .bars line:nth-child(2)").style.stroke = "#ffe2bb";
 
     let bar3Loop = true;
 
     setInterval(function () {
       if (bar3Loop == true) {
-        let bar3a = document.querySelector("#barchart3 .bars line:nth-child(1)");
+        let bar3a = document.querySelector("#barchart3 .bars line:nth-child(2)");
         let comp3a = window.getComputedStyle(bar3a).getPropertyValue("transform");
 
         var values3a = comp3a.split('(')[1];
         values3a = values3a.split(')')[0];
         values3a = values3a.split(', ');
         var bar3aVal = values3a[5];
-        console.log(bar3aVal);
-        vaerdi3a.textContent = `${100 - Math.round(bar3aVal)}%`;
-        slider3a.value = `${1000 - (bar3aVal * 10)}`;
+        vaerdi3a.textContent = `${Math.round(bar3aVal - 7.7)}%`;
+        slider3a.value = `${(bar3aVal * 10) - 77}`;
 
-        let bar3b = document.querySelector("#barchart3 .bars line:nth-child(2)");
+        let bar3b = document.querySelector("#barchart3 .bars line:nth-child(1)");
         let comp3b = window.getComputedStyle(bar3b).getPropertyValue("transform");
 
         var values3b = comp3b.split('(')[1];
         values3b = values3b.split(')')[0];
         values3b = values3b.split(', ');
         var bar3bVal = values3b[5];
-        console.log(bar3bVal);
-        vaerdi3b.textContent = `${100 - Math.round(bar3bVal)}%`;
-        slider3b.value = `${1000 - (bar3bVal * 10)}`;
+        vaerdi3b.textContent = `${Math.round(bar3bVal - 7.7)}%`;
+        slider3b.value = `${(bar3bVal * 10) - 77}`;
       }
     }, 20)
 
@@ -552,4 +586,185 @@ function testYourself1() {
       bar3Loop = false;
     }, 1000)
   });
+}
+
+
+window.onscroll = function () {
+  if (randomRunning == false) {
+    console.log("on scroll");
+    randomRunning = true;
+    window.requestAnimationFrame(function () {
+      randomDataPos1();
+      randomDataPos2();
+      randomDataPos3();
+    })
+  }
+}
+
+
+function randomDataPos1() {
+  var slider1a = document.querySelector("#slider1a");
+  var slider1b = document.querySelector("#slider1b");
+  var vaerdi1a = document.querySelector("#vaerdi1a");
+  var vaerdi1b = document.querySelector("#vaerdi1b");
+
+
+  if (randomRunning == true) {
+    let bar1a_rdm = document.querySelector("#barchart1 .bars line:nth-child(2)");
+    let bar1b_rdm = document.querySelector("#barchart1 .bars line:nth-child(1)");
+    let random1 = Math.round(Math.random() * 100);
+    let random1del = Math.round(Math.random() * 1000 + 1000);
+
+    document.querySelectorAll("#barchart1 .bars line").forEach((bar) => {
+      bar.style.transition = `${random1del / 1000}s cubic-bezier(0.76, 0, 0.24, 1)`;
+    })
+
+    bar1a_rdm.style.transform = `translateY(${random1}px)`;
+    bar1b_rdm.style.transform = `translateY(${100 - random1}px)`;
+    bar1a_rdm.style.stroke = "#3c6196";
+    bar1b_rdm.style.stroke = "#093A82";
+
+
+    let comp1a_rdm = window.getComputedStyle(bar1a_rdm).getPropertyValue("transform");
+    let comp1b_rdm = window.getComputedStyle(bar1b_rdm).getPropertyValue("transform");
+
+    setInterval(function () {
+      if (randomRunning == true) {
+        bar1a_rdm = document.querySelector("#barchart1 .bars line:nth-child(2)");
+        bar1b_rdm = document.querySelector("#barchart1 .bars line:nth-child(1)");
+        comp1a_rdm = window.getComputedStyle(bar1a_rdm).getPropertyValue("transform");
+        comp1b_rdm = window.getComputedStyle(bar1b_rdm).getPropertyValue("transform");
+
+        var values1a_rdm = comp1a_rdm.split('(')[1];
+        values1a_rdm = values1a_rdm.split(')')[0];
+        values1a_rdm = values1a_rdm.split(', ');
+        var bar1a_rdmVal = values1a_rdm[5];
+        vaerdi1a.textContent = `${Math.round(bar1a_rdmVal)}%`;
+        slider1a.value = `${bar1a_rdmVal * 10}`;
+
+        var values1b_rdm = comp1b_rdm.split('(')[1];
+        values1b_rdm = values1b_rdm.split(')')[0];
+        values1b_rdm = values1b_rdm.split(', ');
+        var bar1b_rdmVal = values1b_rdm[5];
+        vaerdi1b.textContent = `${Math.round(bar1b_rdmVal)}%`;
+        slider1b.value = `${(bar1b_rdmVal * 10)}`;
+      }
+    }, 20)
+
+    setTimeout(function () {
+      randomDataPos1();
+    }, random1del)
+  }
+}
+
+function randomDataPos2() {
+  var slider2a = document.querySelector("#slider2a");
+  var slider2b = document.querySelector("#slider2b");
+  var vaerdi2a = document.querySelector("#vaerdi2a");
+  var vaerdi2b = document.querySelector("#vaerdi2b");
+
+
+  if (randomRunning == true) {
+    let bar2a_rdm = document.querySelector("#barchart2 .bars line:nth-child(2)");
+    let bar2b_rdm = document.querySelector("#barchart2 .bars line:nth-child(1)");
+    let random2 = Math.round(Math.random() * 47.9);
+    let random2del = Math.round(Math.random() * 1000 + 1000);
+
+    document.querySelectorAll("#barchart2 .bars line").forEach((bar) => {
+      bar.style.transition = `${random2del / 1000}s cubic-bezier(0.76, 0, 0.24, 1)`;
+    })
+
+    bar2a_rdm.style.transform = `translateY(${random2 + 52.1}px)`;
+    bar2b_rdm.style.transform = `translateY(${47.9 - random2 + 52.1}px)`;
+    bar2a_rdm.style.stroke = "#3c6196";
+    bar2b_rdm.style.stroke = "#093A82";
+
+
+    let comp2a_rdm = window.getComputedStyle(bar2a_rdm).getPropertyValue("transform");
+    let comp2b_rdm = window.getComputedStyle(bar2b_rdm).getPropertyValue("transform");
+
+
+    setInterval(function () {
+      if (randomRunning == true) {
+        bar2a_rdm = document.querySelector("#barchart2 .bars line:nth-child(2)");
+        bar2b_rdm = document.querySelector("#barchart2 .bars line:nth-child(1)");
+        comp2a_rdm = window.getComputedStyle(bar2a_rdm).getPropertyValue("transform");
+        comp2b_rdm = window.getComputedStyle(bar2b_rdm).getPropertyValue("transform");
+
+        var values2a_rdm = comp2a_rdm.split('(')[1];
+        values2a_rdm = values2a_rdm.split(')')[0];
+        values2a_rdm = values2a_rdm.split(', ');
+        var bar2a_rdmVal = values2a_rdm[5];
+        vaerdi2a.textContent = `${Math.round(bar2a_rdmVal - 52.1)}%`;
+        slider2a.value = `${(bar2a_rdmVal * 10) - 521}`;
+
+        var values2b_rdm = comp2b_rdm.split('(')[1];
+        values2b_rdm = values2b_rdm.split(')')[0];
+        values2b_rdm = values2b_rdm.split(', ');
+        var bar2b_rdmVal = values2b_rdm[5];
+        vaerdi2b.textContent = `${Math.round(bar2b_rdmVal - 52.1)}%`;
+        slider2b.value = `${(bar2b_rdmVal * 10) - 521}`;
+      }
+    }, 20)
+
+    setTimeout(function () {
+      randomDataPos2();
+    }, random2del)
+  }
+}
+
+function randomDataPos3() {
+  var slider3a = document.querySelector("#slider3a");
+  var slider3b = document.querySelector("#slider3b");
+  var vaerdi3a = document.querySelector("#vaerdi3a");
+  var vaerdi3b = document.querySelector("#vaerdi3b");
+
+
+  if (randomRunning == true) {
+    let bar3a_rdm = document.querySelector("#barchart3 .bars line:nth-child(2)");
+    let bar3b_rdm = document.querySelector("#barchart3 .bars line:nth-child(1)");
+    let random3 = Math.round(Math.random() * 92.3);
+    let random3del = Math.round(Math.random() * 1000 + 1000);
+
+    document.querySelectorAll("#barchart3 .bars line").forEach((bar) => {
+      bar.style.transition = `${random3del / 1000}s cubic-bezier(0.76, 0, 0.24, 1)`;
+    })
+
+    bar3a_rdm.style.transform = `translateY(${random3 + 7.7}px)`;
+    bar3b_rdm.style.transform = `translateY(${92.3 - random3 + 7.7}px)`;
+    bar3a_rdm.style.stroke = "#3c6196";
+    bar3b_rdm.style.stroke = "#093A82";
+
+
+    let comp3a_rdm = window.getComputedStyle(bar3a_rdm).getPropertyValue("transform");
+    let comp3b_rdm = window.getComputedStyle(bar3b_rdm).getPropertyValue("transform");
+
+
+    setInterval(function () {
+      if (randomRunning == true) {
+        bar3a_rdm = document.querySelector("#barchart3 .bars line:nth-child(2)");
+        bar3b_rdm = document.querySelector("#barchart3 .bars line:nth-child(1)");
+        comp3a_rdm = window.getComputedStyle(bar3a_rdm).getPropertyValue("transform");
+        comp3b_rdm = window.getComputedStyle(bar3b_rdm).getPropertyValue("transform");
+
+        var values3a_rdm = comp3a_rdm.split('(')[1];
+        values3a_rdm = values3a_rdm.split(')')[0];
+        values3a_rdm = values3a_rdm.split(', ');
+        var bar3a_rdmVal = values3a_rdm[5];
+        vaerdi3a.textContent = `${Math.round(bar3a_rdmVal - 7.7)}%`;
+        slider3a.value = `${(bar3a_rdmVal * 10) - 77}`;
+
+        var values3b_rdm = comp3b_rdm.split('(')[1];
+        values3b_rdm = values3b_rdm.split(')')[0];
+        values3b_rdm = values3b_rdm.split(', ');
+        var bar3b_rdmVal = values3b_rdm[5];
+        vaerdi3b.textContent = `${Math.round(bar3b_rdmVal - 7.7)}%`;
+        slider3b.value = `${(bar3b_rdmVal * 10) - 77}`;
+      }
+    }, 20)
+
+    setTimeout(function () {
+      randomDataPos3();
+    }, random3del)
+  }
 }
